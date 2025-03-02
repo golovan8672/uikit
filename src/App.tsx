542127072817f271f21b7d82b35c09263d4c6318
golovan8672 <golovan8672@gmail.com>
2025-03-02 19:21:17 +0500

    add dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
+import Dropdown from './components/Dropdown';
+
+const options = [
+  {
+    id: 1,
+    content: 'Some content for first item',
+    onClick: () => console.log('first'),
+  },
+  {
+    id: 2,
+    content: 'Some content for second item',
+    onClick: () => console.log('second'),
+  },
+  {
+    id: 3,
+    content: 'Some content for third item',
+    onClick: () => console.log('third'),
+  },
+];
+
 function App() {
   return (
-    <>
-      <span>Hello</span>
-    </>
+    <Dropdown>
+      <Dropdown.Trigger>
+        {(props) => <button {...props}>{'Open dropdown'}</button>}
+      </Dropdown.Trigger>
+      <Dropdown.Menu>
+        {options.map(({onClick, content, id}) => (
+          <Dropdown.Item
+            key={id}
+            onClick={onClick}
+          >
+            {content}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
   );
 }
 
